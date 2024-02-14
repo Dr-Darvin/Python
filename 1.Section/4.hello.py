e12a2d0731841c47ddc163240a12b7c3a0a6508b
Dr-Darvin <isuru.etam@gmail.com>
2024-02-14 06:38:09 +0530

    corrected hello.py line 6

--- 1.Section/4.hello.py
+++ 1.Section/4.hello.py
@@ -3,6 +3,6 @@
 print("Hello what is your name?") # print is a fuction and we can pass arguments with in ""
 myName = input()
 print("Hello it's good to meet you " + myName)
-print("Whatis your age?")
+print("What is your age?")
 age = input()
 print("Your age will be "+str(int(age)+5)+" after 5 years") # refer "5.function section"
