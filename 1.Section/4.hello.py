ca9c54760f3933155cc5d735c4b00075a218c3d7
Dr-Darvin <isuru.etam@gmail.com>
2024-02-14 06:31:15 +0530

    add first section

---
+++ 1.Section/4.hello.py
@@ -0,0 +1,8 @@
+# this programe asks the age and say hello
+
+print("Hello what is your name?") # print is a fuction and we can pass arguments with in ""
+myName = input()
+print("Hello it's good to meet you " + myName)
+print("Whatis your age?")
+age = input()
+print("Your age will be "+str(int(age)+5)+" after 5 years") # refer "5.function section"
