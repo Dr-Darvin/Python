956e281c5b2784891ffcc58e884246982a1ad2dd
Dr-Darvin <isuru.etam@gmail.com>
2024-02-14 06:43:18 +0530

    add alternative method in hello.py line 10

--- 1.Section/4.hello.py
+++ 1.Section/4.hello.py
@@ -5,4 +5,6 @@ myName = input()
 print("Hello it's good to meet you " + myName)
 print("What is your age?")
 age = input()
-print("Your age will be "+str(int(age)+5)+" after 5 years") # refer "5.function section"
+print("Your age will be ",str(int(age)+5)," after 5 years") # refer "5.function section"
+# or we can use as follows, by using a "," insted of a '+'
+# print("Your age will be ",str(int(age)+5)," after 5 years")
