6f24db49bee96cb6d0ba5fb7d6dfff28c5fa296d
dr-darvin <isuru.etam@gmail.com>
2024-02-23 09:01:34 +0530

    add elif file

---
+++ 2.section/7.elif.py
@@ -0,0 +1,14 @@
+name="Alice"
+age=35
+
+if name=="Bob":
+    print("Hello bob..")
+
+elif age>=35:
+    print("you're old")
+
+elif age<34:
+    print("you're young ")
+
+else:
+    print("not a humans")
